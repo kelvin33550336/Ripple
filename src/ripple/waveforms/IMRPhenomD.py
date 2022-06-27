410d9931094ec2c2c8be2bd72d723820d6c79c54
kelvin33550336 <99474920+kelvin33550336@users.noreply.github.com>
2022-06-27 17:02:28 -0400

    Coeffs differentiable
    Made waveform differentiable against coeffs

--- src/ripple/waveforms/IMRPhenomD.py
+++ src/ripple/waveforms/IMRPhenomD.py
@@ -463,7 +463,7 @@ def get_IIb_Amp(fM_s: Array, theta: Array, coeffs: Array, f_RD, f_damp) -> Array
 
 
 @jax.jit
-def Phase(f: Array, theta: Array) -> Array:
+def Phase(f: Array, theta: Array, coeffs: Array) -> Array:
     """
     Computes the phase of the PhenomD waveform following 1508.07253.
     Sets time and phase of coealence to be zero.
@@ -478,8 +478,6 @@ def Phase(f: Array, theta: Array) -> Array:
     m1_s = m1 * gt
     m2_s = m2 * gt
     M_s = m1_s + m2_s
-
-    coeffs = get_coeffs(theta)
 
     # Next we need to calculate the transition frequencies
     f1, f2, _, _, f_RD, f_damp = get_transition_frequencies(theta, coeffs[5], coeffs[6])
@@ -542,7 +540,7 @@ def Phase(f: Array, theta: Array) -> Array:
 
 
 @jax.jit
-def Amp(f: Array, theta: Array, D=1) -> Array:
+def Amp(f: Array, theta: Array, coeffs: Array, D=1) -> Array:
     """
     Computes the amplitude of the PhenomD frequency domain waveform following 1508.07253.
     Note that this waveform also assumes that object one is the more massive.
@@ -559,8 +557,6 @@ def Amp(f: Array, theta: Array, D=1) -> Array:
     m2_s = m2 * gt
     M_s = m1_s + m2_s
     eta = m1_s * m2_s / (M_s ** 2.0)
-
-    coeffs = get_coeffs(theta)
 
     _, _, f3, f4, f_RD, f_damp = get_transition_frequencies(theta, coeffs[5], coeffs[6])
 
